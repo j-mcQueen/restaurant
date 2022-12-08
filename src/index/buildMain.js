cca666dd85eea9dee7630064994ae32c35eab439
jack mcqueen <jdmcqueen44@gmail.com>
2022-12-08 16:06:23 +0000

    Add main element and section elements with id attributes

--- src/index/buildMain.js
+++ src/index/buildMain.js
@@ -1,5 +1,27 @@
 const buildMain = () => {
+    const content = document.getElementById("content");
+    const main = document.createElement("main");
+    content.appendChild(main);
 
+    const addSection = (val, elem) => {
+        elem.id = val;
+        main.appendChild(elem);
+    };
+
+    for (let i = 0; i < 3; i++) {
+        let section = document.createElement("section");
+        switch (i) {
+            case 0:
+                addSection("hero", section);
+                continue;
+            case 1:
+                addSection("blurbs", section);
+                continue;
+            case 2:
+                addSection("question", section);
+                break;
+        };
+    };
 };
 
 export { buildMain };
