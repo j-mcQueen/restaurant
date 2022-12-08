f8562973e3d48e0d3e60dc6bfa2f1839bd534294
jack mcqueen <jdmcqueen44@gmail.com>
2022-12-08 15:16:59 +0000

    Add header constituent element styles

--- src/buildHeader.js
+++ src/buildHeader.js
@@ -15,11 +15,13 @@ const buildHeader = () => {
         d.appendChild(elem);
     };
 
-    const addLogo = (d, val, text) => {
+    const addLogo = (d, val, text, num) => {
         let home = document.getElementById("home");
         home.appendChild(d);
         d.id = val;
         d.textContent = text;
+        d.setAttribute("style", `font-size: 28px; font-weight: 700; letter-spacing: -1.25px;
+                                 margin: -1.5px; color: var(--blue); transform: rotate(${num});`);
     };
 
     const addListItem = (a, val, text) => {
@@ -29,23 +31,27 @@ const buildHeader = () => {
         a.id = val;
         a.textContent = text;
         listItem.appendChild(a);
+        a.onmouseover = () => a.style.textDecoration = "underline";
+        a.onmouseleave = () => a.style.textDecoration = "none";
     };
     // create divs + anchors + list items
     for (let i = 0; i < 4; i++) {
         let div = document.createElement("div");
         let anchor = document.createElement("a");
+        anchor.setAttribute("style", "cursor: pointer; color: var(--blue);");
 
         switch (i) {
             case 0:
                 appendElems(div, "logo", anchor);
                 anchor.id = "home";
+                anchor.setAttribute("style", "display: flex; cursor: pointer;");
                 continue;
             case 1:
-                addLogo(div, "lo", "LO");
+                addLogo(div, "lo", "LO", "-45deg");
                 addListItem(anchor, "about", "ABOUT");
                 continue;
             case 2:
-                addLogo(div, "go", "GO");
+                addLogo(div, "go", "GO", "45deg");
                 addListItem(anchor, "menus", "MENUS");
                 continue;
             case 3:
@@ -54,6 +60,11 @@ const buildHeader = () => {
                 break;
         };
     };
+
+    links.setAttribute("style", `display: flex; justify-content: space-evenly; width: 50vw;
+                                 font-weight: bold; font-style: italic; letter-spacing: -1.25px;
+                                 margin: 10px 0 10px 0;`);
+    nav.setAttribute("style", "display: flex; justify-content: space-evenly; padding: 15px 0;");
 };
 
 export { buildHeader, };
