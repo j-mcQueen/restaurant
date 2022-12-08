f8fb78c86d7e730df687294ed5e1616c2c093554
jack mcqueen <jdmcqueen44@gmail.com>
2022-12-08 16:11:45 +0000

    Import buildHero function and call

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
 import { buildHeader } from "./buildHeader";
 import { buildMain } from "./index/buildMain";
+import { buildHero } from "./index/buildHero";
 
 buildHeader();
 buildMain();
+buildHero();
