5ac8ba9dca6205684db9080eaf167affc242cef3
jack mcqueen <jdmcqueen44@gmail.com>
2022-12-12 17:14:29 +0000

    Import buildBlurbs; call buildBlurbs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import "./style.css";
 import { buildHeader } from "./buildHeader";
 import { buildMain } from "./index/buildMain";
 import { buildHero } from "./index/buildHero";
+import { buildBlurbs } from "./index/buildBlurbs";
 
 buildHeader();
 buildMain();
 buildHero();
+buildBlurbs();
