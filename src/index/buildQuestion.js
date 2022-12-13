4d9a5b209c0a1ef086d7711ff1baf58245bc888f
jack mcqueen <jdmcqueen44@gmail.com>
2022-12-13 17:17:07 +0000

    Set styles for comprising elements

--- src/index/buildQuestion.js
+++ src/index/buildQuestion.js
@@ -8,6 +8,11 @@ const buildQuestion = () => {
         subtitle.textContent = title;
         para.textContent = text;
 
+        question.setAttribute("style", `display: flex; flex-direction: column; justify-content: center; align-items: 
+                                        center; gap: 30px; margin-bottom: 10%;`);
+        subtitle.setAttribute("style", `background-color: var(--white); border: 1px solid var(--black); border-radius: 10px; color: var(--black); font-size: 30px; letter-spacing: -1.75px; margin: 0; padding: 5px 15px;`);
+        para.setAttribute("style", `background-color: var(--white); color: var(--black); font-size: 20px; letter-spacing: -1px; margin: 0; padding: 5px 15px; border: 1px solid var(--black); border-radius: 10px;`);
+
         question.appendChild(subtitle);
         question.appendChild(para);
         buildBtns(question, contact);
