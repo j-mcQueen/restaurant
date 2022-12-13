24786dd5cb2ebae916e17d68c6870394d1d64d3c
jack mcqueen <jdmcqueen44@gmail.com>
2022-12-13 15:55:41 +0000

    Style comprising elements; supply function calls with button cta and color arguments

--- src/index/buildBlurbs.js
+++ src/index/buildBlurbs.js
@@ -10,6 +10,13 @@ const buildBlurbs = () => {
         subtitle.textContent = sub;
         para.textContent = blrb;
 
+        blurbs.setAttribute("style", `display: flex; justify-content: center; gap: 20px; margin-bottom: 10%;`);
+        div.setAttribute("style", `display: flex; flex-direction: column; justify-content: space-between; align-items: center; gap: 50px;
+                                 border: 1px solid var(--black); border-radius: 10px; 
+                                 background-color: ${color}; padding: 5% 4%;`);
+        subtitle.setAttribute("style", `font-size: 22px; letter-spacing: -2px; margin: 0; color: var(--black);`);
+        para.setAttribute("style", `text-align: center; letter-spacing: -0.75px; margin: 0; padding: 0 5%`);
+
         let info = [subtitle, para];
         for (let i = 0; i < info.length; i++) {
             div.appendChild(info[i]);
@@ -17,8 +24,8 @@ const buildBlurbs = () => {
         blurbs.appendChild(div);
         buildBtns(div, cta);
     };
-    createElems("pink", "WHO WE ARE", "short paragraph about the company", "about us");
-    createElems("green", "OUR FOOD", "short paragraph about the type of menu/ cuisine", "menus");
+    createElems("pink", "WHO WE ARE", "short paragraph about the company", "about us", "var(--pink)");
+    createElems("green", "OUR FOOD", "short paragraph about the cuisine", "menus", "var(--green)");
 };
 
 export { buildBlurbs };
