b5f81f6feee7918fbdc999c04f8d06bb5d6c7213
jack mcqueen <jdmcqueen44@gmail.com>
2022-12-13 14:42:44 +0000

    Select blurbs container and create comprising elements

--- src/index/buildBlurbs.js
+++ src/index/buildBlurbs.js
@@ -1,5 +1,10 @@
-buildBlurbs = () => {
-
+const buildBlurbs = () => {
+    const blurbs = document.getElementById("blurbs");
+    const createDiv = () => {
+        let div = document.createElement("div");
+        let subtitle = document.createElement("h3");
+        let para = document.createElement("p");
+    };
 };
 
 export { buildBlurbs };
