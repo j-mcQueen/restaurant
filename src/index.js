867e049d320b74ca043d9c05b7f52d4889ee3a4d
jack mcqueen <jdmcqueen44@gmail.com>
2022-12-13 15:58:43 +0000

    Import buildQuestion and call

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import { buildHeader } from "./buildHeader";
 import { buildMain } from "./index/buildMain";
 import { buildHero } from "./index/buildHero";
 import { buildBlurbs } from "./index/buildBlurbs";
+import { buildQuestion } from "./index/buildQuestion";
 
 buildHeader();
 buildMain();
 buildHero();
 buildBlurbs();
+buildQuestion();
