619d155296034a41f29d73b376f4cdcaa6f89220
jack mcqueen <jdmcqueen44@gmail.com>
2022-12-13 14:48:38 +0000

    Set id on container, add text content to subtitle and paragraph, call function with arguments

--- src/index/buildBlurbs.js
+++ src/index/buildBlurbs.js
@@ -1,10 +1,16 @@
 const buildBlurbs = () => {
     const blurbs = document.getElementById("blurbs");
-    const createDiv = () => {
+    const createElems = (cntr, sub, blrb) => {
         let div = document.createElement("div");
         let subtitle = document.createElement("h3");
         let para = document.createElement("p");
+
+        div.id = cntr;
+        subtitle.textContent = sub;
+        para.textContent = blrb;
     };
+    createElems("pink", "WHO WE ARE", "short paragraph about the company");
+    createDiv("green", "OUR FOOD", "short paragraph about the type of menu/ cuisine");
 };
 
 export { buildBlurbs };
