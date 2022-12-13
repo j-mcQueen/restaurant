1a7a49aa54cd7b6922642b6ef0ea949e3f793350
jack mcqueen <jdmcqueen44@gmail.com>
2022-12-13 14:51:36 +0000

    Organise elements, call buildBtns

--- src/index/buildBlurbs.js
+++ src/index/buildBlurbs.js
@@ -1,6 +1,6 @@
 const buildBlurbs = () => {
     const blurbs = document.getElementById("blurbs");
-    const createElems = (cntr, sub, blrb) => {
+    const createElems = (cntr, sub, blrb, cta) => {
         let div = document.createElement("div");
         let subtitle = document.createElement("h3");
         let para = document.createElement("p");
@@ -8,9 +8,16 @@ const buildBlurbs = () => {
         div.id = cntr;
         subtitle.textContent = sub;
         para.textContent = blrb;
+
+        let info = [subtitle, para];
+        for (let i = 0; i < info.length; i++) {
+            div.appendChild(info[i]);
+        };
+        blurbs.appendChild(div);
+        buildBtns(div, cta);
     };
-    createElems("pink", "WHO WE ARE", "short paragraph about the company");
-    createDiv("green", "OUR FOOD", "short paragraph about the type of menu/ cuisine");
+    createElems("pink", "WHO WE ARE", "short paragraph about the company", "about us");
+    createElems("green", "OUR FOOD", "short paragraph about the type of menu/ cuisine", "menus");
 };
 
 export { buildBlurbs };
