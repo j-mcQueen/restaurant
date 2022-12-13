a07aa9d3f58851bd37d2cfb0ac3b60ea7ac77e53
jack mcqueen <jdmcqueen44@gmail.com>
2022-12-13 16:51:45 +0000

    Create comprising elements and organise; add text content

--- src/index/buildQuestion.js
+++ src/index/buildQuestion.js
@@ -1,5 +1,18 @@
+import { buildBtns } from "../buildBtns";
 const buildQuestion = () => {
+    const question = document.getElementById("question");
+    const createElems = (title, text, contact) => {
+        let subtitle = document.createElement("h3");
+        let para = document.createElement("p");
 
+        subtitle.textContent = title;
+        para.textContent = text;
+
+        question.appendChild(subtitle);
+        question.appendChild(para);
+        buildBtns(question, contact);
+    };
+    createElems("have a question?", "short paragraph about how easy it is to get in touch", "contact us");
 };
 
 export { buildQuestion };
