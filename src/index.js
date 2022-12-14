a66e21fedeee1e67ce34cbfd469cb0c63b04bf81
jack mcqueen <jdmcqueen44@gmail.com>
2022-12-14 13:03:20 +0000

    Remove all imports/ function calls less the stylesheet and buildMain

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 import "./style.css";
-import { buildHeader } from "./buildHeader";
 import { buildMain } from "./index/buildMain";
-import { buildHero } from "./index/buildHero";
-import { buildBlurbs } from "./index/buildBlurbs";
-import { buildQuestion } from "./index/buildQuestion";
-import { buildFooter } from "./buildFooter";
 
-buildHeader();
 buildMain();
-buildHero();
-buildBlurbs();
-buildQuestion();
-buildFooter();
