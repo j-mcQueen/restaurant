213c7e5af1954db1ba9ac46ea9408b12f7b9e2ac
jack mcqueen <jdmcqueen44@gmail.com>
2023-01-16 17:59:33 +0000

    Build primary containers; append them

--- src/contact/buildContact.js
+++ src/contact/buildContact.js
@@ -2,6 +2,11 @@ import { buildHeader } from "../buildHeader";
 buildHeader();
 
 const buildContact = () => {
-
+    const content = document.getElementById("content");
+    const main = document.createElement("main");
+    const grid = document.createElement("div");
+    grid.id = "grid";
+    main.appendChild(grid);
+    content.appendChild(main);
 };
 export { buildContact };
